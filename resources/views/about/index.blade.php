a4f5f7b94336cded6103f712f0d369e131e4712f
Ron Hansen <ron.hansen112@gmail.com>
2016-11-10 13:47:24 +0100

    About us updated

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container">
-		<div class="page-header">
-		  <h1>Über uns<small>wow das ist echt interessant</small></h1>
-		</div>
-	</div>
+    <div class="container">
+        <h2><p>Hey, ich bin Helga die Office Eule</p><img src="/pictures/helga-about.png" class="pull-right"
+                                                          style="width: 30%; height : 30%;"/>
+        </h2>
+        <h2 style="margin-top:50px">
+            <small>Wir haben in der IT da so ein neues Office Portal euch zugänglich gemacht.
+                Meine Aufgabe ist es euch zu zeigen, wie ihr schnell und einfach lernt damit umzugehen.
+            </small>
+        </h2>
+    </div>
 @endsection
